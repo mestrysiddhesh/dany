86701bf2b5b46b1982fd05b74da704a4dabb63a3
Siddhesh Mestry <mestrysiddhesh@yahoo.com>
2016-06-22 15:18:32 +0530

    New commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622044728) do
+ActiveRecord::Schema.define(version: 20160622074258) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
